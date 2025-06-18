a2c74f7f88285443b430261721e9bdf3f20a093b
raythurnvoid <53383860+raythurnvoid@users.noreply.github.com>
2025-06-18 01:59:15 +0100

    react 19

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 import { tanstackRouter } from "@tanstack/router-plugin/vite";
 import path from "node:path";
+import { resolve } from "node:path";
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -19,5 +20,21 @@ export default defineConfig({
 		alias: {
 			"@": path.resolve(__dirname, "./src"),
 		},
+	},
+	server: {
+		// Ignore +personal folder for dev server
+		fs: {
+			deny: ["+personal/**"],
+		},
+		// Exclude +personal folder from being watched, they need to be absolute paths globs
+		watch: {
+			ignored: [resolve("+personal/**")],
+		},
+	},
+	optimizeDeps: {
+		entries: [
+			// Necessary to prevent vite from crawling the `+personal` folder
+			"./index.html",
+		],
 	},
 });
